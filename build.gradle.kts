cc2a21d35a42448358add86881a27cdfed3a4424
Sumit Sarkar <ssarkar@cimpress.com>
2021-01-17 12:26:49 +0530

    CHORE: Upgrade to hotwire

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,13 @@ repositories {
 	mavenCentral()
 }
 
+sourceSets {
+	main {
+		resources.srcDirs("src/main/resources", "dist")
+	}
+}
+
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
